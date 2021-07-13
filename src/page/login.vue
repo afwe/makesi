d12c508aaa28ef997433fb09ce870103dc3e2abd
afwe <936017948@qq.com>
2021-07-13 10:40:18 +0800

    common update

---
+++ src/page/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="mainContainer">
+        <div class="login" v-bind="mode == 'login'">
+        </div>
+        <div class="register" v-bind="mode == 'register'">
+        </div>
+        <div class="forgetPassword" v-bind="mode == 'forgetPassword'">
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default{
+    data(){
+        return{
+            mode: "login",
+            userID: "",
+            userPassword: "",
+            userIden: "",
+            userClass: "",
+            userName: "",
+            userMail: "",
+        }
+    },
+
+}
+</script>
