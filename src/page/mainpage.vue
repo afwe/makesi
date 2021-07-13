853df05c92263c035d1d4565914de0f032a597fe
afwe <936017948@qq.com>
2021-07-13 10:40:08 +0800

    common update

---
+++ src/page/mainpage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="welcome">
+        欢迎学习马克思主义
+    </div>
+    <div class="course-list">
+    </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default {
+
+}
+</script>
