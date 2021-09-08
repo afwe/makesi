0c5761d0a9797bc8dcf8ec259534ca093fa4e3be
afwe <936017948@qq.com>
2021-09-08 12:44:45 +0800

    remove less

--- build/utils.js
+++ build/utils.js
@@ -58,7 +58,6 @@ exports.cssLoaders = function (options) {
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
-    less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
     scss: generateLoaders('sass'),
     stylus: generateLoaders('stylus'),
