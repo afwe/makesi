5922bda2de5c3199d714addb1c9b2227631945b4
afwe <936017948@qq.com>
2021-10-11 09:02:08 +0800

    create course

--- src/page/userInfo.vue
+++ src/page/userInfo.vue
@@ -1,5 +1,7 @@
 <template>
 <div class="pageContainer">
+    <div class="mask" v-show="showMask==true">
+    </div>
     <div class="topHolder-div" :style="userBackground">
         <div class="avator-img">
         </div>
@@ -25,7 +27,7 @@
                 成就
             </div>
         </div>
-        <div class="courseContainer" @click="showCreate=true">
+        <div class="courseContainer" @click="showCreate=true;showMask=true">
             <div class="createCourse-div">
                 <div class="crossSvg" :style="crossBackground">
                 </div>
@@ -40,7 +42,7 @@
             <div class="createDialogTitle-p">
                 课程信息
             </div>
-            <div class="close-img">
+            <div class="close-img" @click="closeDialog">
             </div>
         </div>
         <div class="line">
@@ -49,30 +51,30 @@
             <div class="newTitle-p">
                 名称
             </div>
-            <input class="newCourseTitle">
-            </input>
+            <textarea class="newCourseTitle"  v-model="newCourseTitle">
+            </textarea>
         </div>
         <div class="descSet-div">
             <div class="newDesc-p">
                 简介
             </div>
-            <input class="newCourseDesc">
-            </input>
+            <textarea class="newCourseDesc" v-model="newCourseDesc">
+            </textarea>
         </div>
         <div class="faceSet-div">
             <div class="newFace-p">
                 封面
             </div>
-            <input class="newCourseFace">
-            </input>
+            <div class="newCourseFace">
+            </div>
         </div>
         <div class="line">
         </div>
         <div class="btnNest">
-            <button class="cancel-button">
+            <button class="cancel-button" @click="closeDialog">
                 取消
             </button>
-            <button class="create-button">
+            <button class="create-button" @click="createCourse">
                 创建
             </button>
         </div>
@@ -80,8 +82,7 @@
 </div>
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api'
-
+import {create_course, create_course_identify} from '../fetch/course';
 export default {
     data(){
         return{
@@ -100,10 +101,33 @@ export default {
                 backgroundImage:  'url(' +require('../assets/if-plus.svg')+')',
                 backgroundSize: "100% 100%" 
             },
-            showCreate: false
+            showCreate: false,
+            showMask: false,
+            newCourseTitle:"",
+            newCourseDesc: "",
         }
     },
     methods:{
+        createCourse: async function(){
+            let response;
+            response = await create_course_identify();
+            if(response.code == 200){
+                response = await create_course({
+                    courseName: this.newCourseTitle,
+                    courseIntro: this.newCourseDesc
+                });
+                if(response.code == 200){
+                    this.$message({
+                        message:"课程创建成功",
+                        type:"success"
+                    })
+                }
+            }
+        },
+        closeDialog(){
+            this.showCreate=false;
+            this.showMask=false;
+        },
         saveInfo: async function(){
             let response=await saveInfo({
                 name: this.name,
@@ -136,59 +160,145 @@ export default {
 }
 </script>
 <style scoped>
+.mask{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    z-index: 1009;
+    background-color: rgb(0,0,0,0.2);
+}
 .createDialog-div{
-
+    z-index: 1010;
+    position: fixed;
+    top: calc(50% - 313px);
+    left: calc(50% - 455px);
+    display: flex;
+    flex-flow: column;
+    width: 911px;
+    height: 627px;
+    line-height: 20px;
+    border-radius: 6px;
+    text-align: center; 
+    background-color: white;
 }
 .dialogHead-div{
-    
+        display: flex;
+        flex-flow:row;
 }
 .createDialogTitle-p{
-    
+    margin-left: 45px;
+    margin-top: 18px;
+    line-height: 44px;
+    color: rgba(16, 16, 16, 100);
+    font-size: 30px;
+    text-align: left;
+    font-family: SourceHanSansSC-medium;
 }
 .close-img{
-    
+    margin-top: 12px;
+    margin-left: 688px;
+
 }
 .line{
-    
+    width: 100%;
+    margin-top: 18px;
+    border: 1px solid rgba(224, 224, 224, 100);
 }
 .titleSet-div{
-    
+    margin-top: 41px;
+    display: flex;
+    flex-flow: row;
 }
 .newTitle-p{
-    
+    margin-left: 45px;
+    line-height: 32px;
+    color: rgba(117, 117, 117, 100);
+    font-size: 22px;
+    text-align: left;
+    font-family: SourceHanSansSC-regular;
 }
 .newCourseTitle{
-    
+    margin-left:24px;
+    width: 753px;
+    height: 46px;
+    line-height: 46px;
+    border-radius: 6px;
+    text-align: left;
+    border: 1px solid rgba(187, 187, 187, 100);
+    resize:none;
 }
 .descSet-div{
-    
+    margin-top: 30px;
+    display: flex;
+    flex-flow: row;
 }
 .newDesc-p{
-    
+    margin-left: 45px;
+    width: 44px;
+    line-height: 32px;
+    color: rgba(117, 117, 117, 100);
+    font-size: 22px;
+    text-align: left;
+    font-family: SourceHanSansSC-regular;
 }
 .newCourseDesc{
-    
+    margin-left:24px;
+    width: 753px;
+    height: 105px;
+    line-height: 20px;
+    border-radius: 6px;
+    text-align: left;
+    border: 1px solid rgba(187, 187, 187, 100);
+    resize:none;
 }
 .faceSet-div{
-    
+    margin-top: 30px;
+display: flex;
+flex-flow: row;
 }
 .newFace-p{
-    
+    margin-left: 45px;
+    width: 44px;
+line-height: 32px;
+color: rgba(117, 117, 117, 100);
+font-size: 22px;
+text-align: left;
+font-family: SourceHanSansSC-regular;
 }
 .newCourseFace{
-    
-}
-.line{
-    
+    margin-left:24px;
+    width: 227px;
+height: 142px;
+border-radius: 6px;
 }
 .btnNest{
-    
+    display: flex;
+    flex-flow: row;
+    margin-left: 654px;
+    margin-top:30px;
 }
 .cancel-button{
-    
+    width: 93px;
+height: 52px;
+border-radius: 6px;
+background-color: rgba(238, 238, 238, 100);
+color: rgba(97, 97, 97, 100);
+font-size: 22px;
+text-align: center;
+border: none;
+font-family: Microsoft Yahei;
 }
 .create-button{
-    
+    margin-left:26px;
+    width: 93px;
+height: 52px;
+border-radius: 6px;
+background-color: rgba(255, 5, 47, 100);
+color: rgba(255, 255, 255, 100);
+font-size: 22px;
+text-align: center;
+border:none;
+font-family: Microsoft Yahei;
 }
 .createCourse-div{
     margin-left:calc(50% - 442px);
