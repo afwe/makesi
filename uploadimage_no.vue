472610bf3e5bafe5648cd39781ca8efef7f05dc1
18062706139fcz <90562015+18062706139fcz@users.noreply.github.com>
2022-01-20 10:15:14 +0800

    Update uploadimage_no.vue

--- uploadimage_no.vue
+++ uploadimage_no.vue
@@ -9,14 +9,23 @@
             :http-request="httpRequest"
             multiple
             >
-        <i v-if="videoFlag == false" class="el-icon-plus avatar-uploader-icon "></i>
-        </el-upload>  
+        <i v-if="imageFlag == false" class="el-icon-plus avatar-uploader-icon "></i>
+        </el-upload>   
+        <el-progress
+            v-if="imageFlag"
+            type="circle"
+            :percentage="imageUploadPercent"
+        ></el-progress>
+        <p class="Upload_pictures">
+            <span>图片显示4:3,格式jpg</span>
+        </p>
     </div>
 </template>
 <script>
   import {uploadCourseFace} from '../fetch/upload'
   import COS from 'cos-nodejs-sdk-v5'
   import {getKey} from '../fetch/image'
+
   export default {
     name: 'imgUpload',
     data () {
@@ -50,18 +59,20 @@
                     });
                 }
             });
-            var fileKey
-            fileKey = Date.now();
+            // var fileKey
+            // fileKey = Date.now();
             const uploader = cos.putObject({
+
+                //存储桶的名称，命名规则为 BucketName-APPID，此处填写的存储桶名称必须为此格式
                 Bucket: 'red-video-1305720498', /* 必须 */
                 //存储桶所在地域
                 Region: 'red-video-1305720498.cos.ap-nanjing.myqcloud.com',    /* 必须 */
                 //对象键（Object 的名称），对象在存储桶中的唯一标识，了解更多请参见
                 Key: 'red-video-1305720498.cos.ap-nanjing.myqcloud.com/course_pic.jpg',              /* 必须 */
-                Body: file.file, // 这里腾讯云需要获取到file文件里的name，根据你file结构进行填写
                 StorageClass: 'STANDARD',
+                Body: file.file, // 这里腾讯云需要获取到file文件里的name，根据你file结构进行填写
                 onProgress: function(info) {
-                    _this.videoUploadPercent = parseInt(info.percent * 100);
+                    _this.imageUploadPercent = parseInt(info.percent * 100);
                 }
                 // mediaFile: file.file // 这里腾讯云需要获取到file文件里的name，根据你file结构进行填写
             }, ( err , doneResult ) => {
@@ -88,6 +99,7 @@
         }
     }
   }
+
 </script>
 
 <style scoped>
@@ -95,5 +107,7 @@
     width:300px;
     height:200px;
 }
-
 </style>
+
+
+
